0f726d8a344ddf60f296ef71e0ebe75f04a012f7
adrian77720 <adrianszmajewski8@gmail.com>
2025-03-29 08:00:25 +0000

    Added text scaling for the mobile view, Implemented automatic closing of the navigation bar after exiting the menu, Conducted functionality testing, Updated JavaScript codebase, Improved performance and stability

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -15,3 +15,39 @@
                 footerText.innerHTML = `&copy; ${new Date().getFullYear()} MarekLux. ${messages[index]}`;
             }, 5000);
         });
+
+        document.addEventListener("DOMContentLoaded", function () {
+            var navbarMenu = document.querySelector("#navbarMenu");
+            var containerFluid = document.querySelector(".container-fluid");
+    
+            // Tworzymy instancję Collapse
+            var bsCollapse = new bootstrap.Collapse(navbarMenu, { toggle: false });
+    
+            // Funkcja do opóźnienia
+            function delay(ms) {
+                return new Promise(function(resolve) {
+                    setTimeout(resolve, ms);
+                });
+            }
+    
+            // Sprawdzamy, czy kursor jest nad elementem container-fluid
+            function isMouseOverContainerFluid(event) {
+                return containerFluid.contains(event.target);
+            }
+    
+            // Dodatkowa obsługa dla zamykania menu, gdy kursor nie jest nad container-fluid
+            document.addEventListener("mousemove", function(event) {
+                // Jeśli kursor opuścił container-fluid
+                if (!isMouseOverContainerFluid(event)) {
+                    console.log("Kursor opuścił container-fluid, czekam 500ms przed zamknięciem.");
+    
+                    delay(500).then(function () {
+                        // Jeśli kursor nadal nie jest nad container-fluid, zamknij menu
+                        if (!isMouseOverContainerFluid(event)) {
+                            console.log("Zamykam menu, kursor nie jest nad container-fluid.");
+                            bsCollapse.hide(); // Zamykanie menu
+                        }
+                    });
+                }
+            });
+        });
